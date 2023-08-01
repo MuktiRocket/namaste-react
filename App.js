80cf18d92bdd325242a70ffc210ffcfba38187af
MuktiRocket <jaydeeps96s@gmail.com>
2023-08-01 10:11:48 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,42 @@
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement(
+        "div",
+        { id: "child" },
+        [
+            React.createElement(
+                "h1",
+                {},
+                "Iam h1 Tag"
+            ),
+            React.createElement(
+                "h2",
+                {},
+                "Iam h2 Tag"
+            )
+        ],
+
+    ),
+    React.createElement(
+        "div",
+        { id: "child2" },
+        [
+            React.createElement(
+                "h1",
+                {},
+                "Iam h1 Tag"
+            ),
+            React.createElement(
+                "h2",
+                {},
+                "Iam h2 Tag"
+            )
+        ],
+
+    )
+);
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello World! from React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
