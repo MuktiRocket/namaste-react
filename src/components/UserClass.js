117345bf11c132529e19e1c1c2c24971c8c65a24
MuktiRocket <jaydeeps96s@gmail.com>
2023-08-29 00:37:27 +0530

    class based components

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,27 +4,30 @@ export default class UserClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
+      userInfo: {
+        name: "Dummy User",
+        location: "Default location",
+        Company: "Dummy Company",
+        avatar_url: "https://dummyuser.com",
+      },
     };
   }
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/akshaymarch7");
+    const json = await data.json();
+    this.setState({
+      userInfo: json,
+    });
+  }
   render() {
-    const { name, location } = this.props;
-    const { count } = this.state;
+    // const { name, location } = this.props;
+    const { name, location, company, avatar_url } = this.state.userInfo;
     return (
       <div className="user-card">
-        <h1>Count: {count}</h1>
-        <button
-          onClick={() => {
-            this.setState({
-              count: this.state.count + 1,
-            });
-          }}
-        >
-          Count Increase
-        </button>
+        <img src={avatar_url} />
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
-        <h4>Contact: jaydeeps96</h4>
+        <h4>Company: {company}</h4>
       </div>
     );
   }
