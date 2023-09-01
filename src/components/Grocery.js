fcdaff21acc7f6642c45f1d40040e2238d1cd050
pc121 <nilanjankamila@gmail.com>
2023-09-01 14:52:04 +0530

    optimising the app

---
+++ src/components/Grocery.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Grocery = (props) => {
+  return (
+    <div className="grocery-card">
+      <h2>Grocery Page</h2>
+    </div>
+  );
+};
+
+export default Grocery;
