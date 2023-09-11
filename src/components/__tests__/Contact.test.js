32725aec7bc78d9f39427f7ac5836b99baa7d867
pc121 <nilanjankamila@gmail.com>
2023-09-11 14:50:53 +0530

    added test cases jest unit testing

---
+++ src/components/__tests__/Contact.test.js
@@ -0,0 +1,29 @@
+import { screen, render } from "@testing-library/react";
+import Contact from "../Contact";
+import "@testing-library/jest-dom";
+
+describe("Contact us page test case", () => {
+  it("should load contact component", () => {
+    render(<Contact />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("should load button inside contact component", () => {
+    render(<Contact />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+  });
+
+  test("should load input name inside contact component", () => {
+    render(<Contact />);
+    const inputName = screen.getByPlaceholderText("name");
+    expect(inputName).toBeInTheDocument();
+  });
+
+  test("should load 2 input boxes in contact component", () => {
+    render(<Contact />);
+    const inputBoxes = screen.getAllByRole("textbox");
+    expect(inputBoxes.length).toBe(2);
+  });
+});
