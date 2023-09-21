595212867640b8b01ebb39b5a648a937c1511bda
pc121 <nilanjankamila@gmail.com>
2023-09-21 14:33:13 +0530

    res card testing

---
+++ src/components/__tests__/RestaurantCard.test.js
@@ -0,0 +1,7 @@
+import { render } from "@testing-library/react";
+import { RestaurantCard } from "../RestaurantCard";
+
+it("should render restaurant card with props data", () => {
+  render(<RestaurantCard />);
+  //2:31
+});
